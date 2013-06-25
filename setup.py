159b00601c5d9dd8876413dbf99847c6a1ac9cec
Thijs Jonkman <t.jonkman@gmail.com>
2013-06-25 23:27:21 +0200

    Initial package structure

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='diazotheme.plone',
+      version=version,
+      description="",
+      long_description=open("README.md").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='TH-code',
+      author_email='t.jonkman@gmail.com',
+      url='https://github.com/TH-code',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['diazotheme'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'diazoframework.plone',
+          'diazoframework.purecss',
+      ],
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
